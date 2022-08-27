656c0c818f19b34c4142980209b3dacd5e910b7f
Guilherme Vinicius <guilherme@fieldcontrol.com.br>
2022-08-27 13:37:31 -0300

    chore: initial commit

---
+++ model/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+    mode: process.env.ENVIRONMENT == 'production' ? 'production' : 'development',
+    entry: './styles/index.scss',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+};
